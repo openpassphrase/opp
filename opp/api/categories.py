dd2d1b14191db44500503003beb95dc45a3cc7a1
Alexander Bashmakov <alexander.bashmakov@intel.com>
2016-12-28 22:23:50 +0000

    Initial commit

---
+++ opp/api/categories.py
@@ -0,0 +1,117 @@
+#!/usr/local/bin/python
+
+
+import base_handler
+from common import aescipher
+from common import utils
+
+
+class ResponseHandler(base_handler.BaseResponseHandler):
+
+    def _handle_getall(self, phrase):
+        response = []
+        cipher = aescipher.AESCipher(phrase)
+        self.db_cursor.execute("SELECT * from categories ORDER by id")
+        categories = self.db_cursor.fetchall()
+        for cat_id, category_blob in categories:
+            cat = cipher.decrypt(category_blob)
+            response.append({'id': cat_id, 'category': cat})
+
+        return {'result': 'success', 'categories': response}
+
+    def _handle_create(self, phrase):
+        cat_list, error = self._get_payload()
+        if error:
+            return error
+
+        cipher = aescipher.AESCipher(phrase)
+        payload = []
+        for cat in cat_list:
+            if not cat:
+                resp = {'category': None,
+                        'status': "empty category not allowed"}
+            else:
+                # Encrypt category name blob
+                cat_blob = cipher.encrypt(cat)
+
+                # Store category name blob
+                sql = ("INSERT INTO categories (category_blob)"
+                       "VALUES (%s)" % utils.qq(cat_blob))
+                if self.db_cursor.execute(sql) == 1:
+                    resp = {'category': cat,
+                            'status': "success: created"}
+                else:
+                    resp = {'category': cat,
+                            'status': "error: failed to create"}
+
+            payload.append(resp)
+
+        return {'result': 'success', 'payload': payload}
+
+    def _handle_update(self, phrase):
+        cat_list, error = self._get_payload()
+        if error:
+            return error
+
+        payload = []
+        cipher = aescipher.AESCipher(phrase)
+        for cat in cat_list:
+            if not cat:
+                # Silently ignore any empty dictionaries
+                continue
+            try:
+                # Make sure category id is parsed from request
+                cat_id = cat['id']
+            except KeyError:
+                cat_id = None
+
+            if not cat_id:
+                cat['status'] = "error: missing or empty category id"
+            else:
+                try:
+                    # Make sure category is parsed from request
+                    category = cat['category']
+                except KeyError:
+                    category = None
+
+                if not category:
+                    cat['status'] = "error: missing or empty category"
+                else:
+                    cat_blob = utils.qq(cipher.encrypt(category))
+                    sql = ("UPDATE categories SET category_blob = %s"
+                           "WHERE id=%s" % (cat_blob, cat_id))
+                if self.db_cursor.execute(sql) == 1:
+                    cat['status'] = "success: updated"
+                else:
+                    cat['status'] = "error: does not exist"
+
+            payload.append(cat)
+
+        return {'result': 'success', 'payload': payload}
+
+    def _handle_delete(self, phrase):
+        cat_list, error = self._get_payload()
+        if error:
+            return error
+
+        payload = []
+        for cat_id in cat_list:
+            if not cat_id:
+                resp = {'id': None,
+                        'status': "error: empty category id"}
+            else:
+                sql = "DELETE FROM categories WHERE id=%s" % cat_id
+                if self.db_cursor.execute(sql) == 1:
+                    resp = {'id': cat_id,
+                            'status': "success: deleted"}
+                else:
+                    resp = {'id': cat_id,
+                            'status': "error: does not exist"}
+
+                # TODO(alex_bash): report # entries removed to user?
+                sql = "DELETE FROM entries WHERE category_id=%s" % cat_id
+                self.db_cursor.execute(sql)
+
+            payload.append(resp)
+
+        return {'result': 'success', 'payload': payload}
---
+++ opp/api/entries.py
@@ -0,0 +1,146 @@
+#!/usr/local/bin/python
+
+
+import base_handler
+from common import aescipher
+from common import utils
+
+
+class ResponseHandler(base_handler.BaseResponseHandler):
+
+    def _handle_getall(self, phrase):
+        response = []
+        cipher = aescipher.AESCipher(phrase)
+        sql = ("SELECT e.id, e.entry_blob, e.category_id, "
+               "c.category_blob FROM entries AS e LEFT JOIN "
+               "(categories AS c) ON (e.category_id=c.id) "
+               " ORDER BY e.id")
+        self.db_cursor.execute(sql)
+        entries = self.db_cursor.fetchall()
+        for item in entries:
+            resp = {'id': item[0],
+                    'entry': cipher.decrypt(item[1]),
+                    'category_id': item[2]}
+            try:
+                resp['category'] = cipher.decrypt(item[3])
+            except TypeError:
+                resp['category'] = None
+
+            response.append(resp)
+        return {'result': 'success', 'entries': response}
+
+    def _handle_create(self, phrase):
+        entry_list, error = self._get_payload()
+        if error:
+            return error
+
+        cipher = aescipher.AESCipher(phrase)
+        payload = []
+        for item in entry_list:
+            if not item:
+                # Silently ignore any empty dictionaries
+                continue
+            try:
+                # Make sure category id is parsed from request
+                cat_id = item['category_id']
+                if not cat_id:
+                    cat_id = "NULL"
+            except KeyError:
+                cat_id = "NULL"
+
+            try:
+                # Make sure entry is parsed from request
+                entry = item['entry']
+            except KeyError:
+                entry = None
+            if not entry:
+                item['status'] = "error: missing or empty entry"
+            else:
+                # Encrypt entry blob
+                ent_blob = cipher.encrypt(entry)
+
+                # Store entry
+                sql = ("INSERT INTO entries (category_id,"
+                       "entry_blob) VALUES (%s,%s)" %
+                       (cat_id, utils.qq(ent_blob)))
+                if self.db_cursor.execute(sql) == 1:
+                    item['status'] = "success: created"
+                else:
+                    item['status'] = "error: failed to create"
+
+            payload.append(item)
+
+        return {'result': 'success', 'payload': payload}
+
+    def _handle_update(self, phrase):
+        entry_list, error = self._get_payload()
+        if error:
+            return error
+
+        payload = []
+        cipher = aescipher.AESCipher(phrase)
+        for ent in entry_list:
+            if not ent:
+                # Silently ignore any empty dictionaries
+                continue
+            try:
+                # Make sure entry id is parsed from request
+                ent_id = ent['id']
+            except KeyError:
+                ent_id = None
+
+            if not ent_id:
+                ent['status'] = "error: missing or empty entry id"
+            else:
+                try:
+                    # Make sure entry is parsed from request
+                    entry = ent['entry']
+                except KeyError:
+                    entry = None
+                if not entry:
+                    ent['status'] = "error: missing or empty entry"
+                else:
+                    entry_blob = utils.qq(cipher.encrypt(entry))
+                    try:
+                        cat_id = ent['category_id']
+                        if not cat_id:
+                            cat_id = "NULL"
+                    except KeyError:
+                        cat_id = "NULL"
+                    sql = ("UPDATE entries SET "
+                           "entry_blob = %s, "
+                           "category_id = %s "
+                           "WHERE id=%s" % (entry_blob,
+                                            cat_id,
+                                            ent_id))
+                    if self.db_cursor.execute(sql) == 1:
+                        ent['status'] = "success: updated"
+                    else:
+                        ent['status'] = "error: does not exist"
+
+            payload.append(ent)
+
+        return {'result': 'success', 'payload': payload}
+
+    def _handle_delete(self, phrase):
+        entry_list, error = self._get_payload()
+        if error:
+            return error
+
+        payload = []
+        for ent_id in entry_list:
+            if not ent_id:
+                resp = {'id': None,
+                        'status': "error: empty entry id not allowed"}
+            else:
+                sql = "DELETE FROM entries WHERE id=%s" % ent_id
+                if self.db_cursor.execute(sql) == 1:
+                    resp = {'id': ent_id,
+                            'status': "success: deleted"}
+                else:
+                    resp = {'id': ent_id,
+                            'status': "error: does not exist"}
+
+            payload.append(resp)
+
+        return {'result': 'success', 'payload': payload}
