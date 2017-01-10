1ad6de4d3569de0f86bc265fdb82cf3b56cb2435
Alexander Bashmakov <alexander.bashmakov@intel.com>
2017-01-10 05:05:58 +0000

    Added functional test for dbmgr.

--- opp/common/utils.py
+++ opp/common/utils.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python
 
 
+import shlex
+import subprocess
 from urlparse import parse_qs
 import uuid
 import wsgiref.util as wsgiutil
@@ -30,3 +32,23 @@ def qq(arg):
 
 def genuuid():
     return str(uuid.uuid4())
+
+
+def execute(cmd):
+    args = shlex.split(cmd)
+    process = subprocess.Popen(args,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    result = process.communicate()
+    (out, err) = result
+    exitcode = process.returncode
+
+    if exitcode != 0:
+        msg = ("Command %(cmd)s did not succeed. Returned an exit "
+               "code of %(exitcode)d."
+               "\n\nSTDOUT: %(out)s"
+               "\n\nSTDERR: %(err)s" % {'cmd': cmd, 'exitcode': exitcode,
+                                        'out': out, 'err': err})
+        raise RuntimeError(msg)
+    return exitcode, out, err
---
+++ opp/tests/functional/test_dbmgr.py
@@ -0,0 +1,46 @@
+
+"""Functional test cases for glance-manage"""
+
+import os
+import tempfile
+import testtools
+
+from opp.common import utils
+
+
+class TestDbManager(testtools.TestCase):
+
+    def setUp(self):
+        super(TestDbManager, self).setUp()
+        self.test_dir = tempfile.mkdtemp(suffix='opp')
+        self.conf_filepath = os.path.join(self.test_dir, 'opp.cfg')
+        self.db_filepath = os.path.join(self.test_dir, 'test.sqlite')
+        self.connection = ("sql_connect: 'sqlite:///%s'" % self.db_filepath)
+
+    def tearDown(self):
+        os.remove(self.conf_filepath)
+        os.remove(self.db_filepath)
+        os.rmdir(self.test_dir)
+        super(TestDbManager, self).tearDown()
+        pass
+
+    def _init_db(self):
+        with open(self.conf_filepath, 'wb') as conf_file:
+            conf_file.write(self.connection)
+            conf_file.flush()
+
+        utils.execute("opp-db --config_file %s init" % self.conf_filepath)
+
+    def _assert_table_exists(self, db_table):
+        cmd = ("sqlite3 {0} \"SELECT name FROM sqlite_master WHERE "
+               "type='table' AND name='{1}'\"").format(self.db_filepath,
+                                                       db_table)
+        code, out, err = utils.execute(cmd)
+        msg = "Expected table {0} was not found in the schema".format(db_table)
+        self.assertEqual(out.rstrip(), db_table, msg)
+
+    def test_schema_creation(self):
+        self._init_db()
+
+        for table in ['categories', 'entries']:
+            self._assert_table_exists(table)
