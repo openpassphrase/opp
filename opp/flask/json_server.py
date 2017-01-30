6b99823df6b84b929915079efd7d362e60241465
Alexander Bashmakov <alexander.bashmakov@intel.com>
2017-01-30 10:53:01 -0700

    Added fetchall endpoint.
    New endpoind returns array of categories with array of items for
    each category. Items without category are placed under ``default``
    category in the response.
    
    Aded unit/functional tests.
    
    ToDo: update documentation.

--- opp/flask/json_server.py
+++ opp/flask/json_server.py
@@ -19,7 +19,7 @@ import logging
 
 from flask import Flask, request
 
-from opp.api.v1 import categories, items
+from opp.api.v1 import categories, fetch_all, items
 from opp.common import opp_config, utils
 from opp.db import api
 from opp.flask.flask_jwt import JWT, jwt_required
@@ -89,6 +89,17 @@ jwt = JWT(app, authenticate, identity)
 @app.route("/v1/health")
 def health_check():
     return _to_json({'status': "OpenPassPhrase service is running"})
+
+
+@app.route("/v1/fetchall")
+@jwt_required()
+def handle_fetchall():
+    err = _enforce_content_type()
+    if err:
+        return err, 400
+    handler = fetch_all.ResponseHandler(request)
+    response = handler.respond()
+    return _to_json(response)
 
 
 @app.route("/v1/categories",
