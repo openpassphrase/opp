234d2c193ebef207ee88fe7c18241adaf9aa95a8
Alexander Bashmakov <abashmak@yahoo.com>
2018-02-21 14:02:42 -0700

    Updated opp_web.sh script with latest libraries and commands.
    Opp-web now uses Yarn for dependency management, so update the script
    to use that. Also install NodeJs 9.x instead of 6.x.

--- docs/source/dev/web_setup.sh
+++ docs/source/dev/web_setup.sh
@@ -2,6 +2,6 @@
 
 # Required packages
 # Add node repository
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install -g angular-cli
+sudo npm install -g yarn 
